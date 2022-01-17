d2e2095c6d6424bf8f900f4ecf857ba173ef6b5f
MoAlyousef <may642_2000@hotmail.com>
2022-01-17 12:33:48 +0300

    apply changes

--- build.rs
+++ build.rs
@@ -3,7 +3,9 @@ use std::env;
 
 fn main() {
     println!("cargo:rerun-if-changed=gui/mainview.fl");
+    println!("cargo:rerun-if-changed=gui/mydialog.fl");
     let g = fl2rust::Generator::default();
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     g.in_out("gui/mainview.fl", out_path.join("mainview.rs").to_str().unwrap()).expect("Failed to generate rust from fl file!");
+    g.in_out("gui/mydialog.fl", out_path.join("mydialog.rs").to_str().unwrap()).expect("Failed to generate rust from fl file!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use fltk::{prelude::*, *};
 mod mainview;
+mod mydialog;
 
 fn main() {
-    let app = app::App::default().with_scheme(app::Scheme::Gtk);
-    let mut ui = mainview::UserInterface::make_window();
-    let mut frame = ui.frame.clone();
-    ui.but.set_callback(move |_| {
+    let app = app::App::default();
+
+    let mut mainwin = mainview::UserInterface::make_window();
+    let mut frame = mainwin.frame.clone();
+
+    mainwin.btn.set_callback(move |_| {
         frame.set_label("Hello World!");
+        let dialog = mydialog::Dialog::make_dialog();
+        while dialog.dialog.shown() {
+            app::wait();
+        }
     });
+
     app.run().unwrap();
 }
